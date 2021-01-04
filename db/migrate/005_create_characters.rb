517832e31c0717411f9c630fafb9dab18ded5f36
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-04 17:51:58 +0000

    Automatically backed up by Learn

---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,11 @@
+class CreateCharacters < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :characters do |t|
+      #primary key of :id is created for us!
+      t.string :first_name
+      t.string :last_name
+    end
+  end
+
+end
