b2bc05c19f29bab2ff53cd34c8f0ca2a72608c98
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-04 17:57:21 +0000

    Automatically backed up by Learn

--- db/migrate/006_adds_catchphrase.rb
+++ db/migrate/006_adds_catchphrase.rb
@@ -3,6 +3,6 @@ class AddCatchPhrase < ActiveRecord::Migration[5.1]
   #a new migration, and then in the change method,
   #we make the change.
   def change
-    add_column :shows, :network_id, :integer
+    add_column :characters, :catchphrase, :string
   end
 end
