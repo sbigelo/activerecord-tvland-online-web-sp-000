cbfe80863c64cd3465303b809cb334c8617a76dd
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-04 17:58:04 +0000

    Automatically backed up by Learn

--- db/migrate/006_adds_catchphrase.rb
+++ db/migrate/006_adds_catchphrase.rb
@@ -1,4 +1,4 @@
-class AddsCatchPhrase < ActiveRecord::Migration[5.1]
+class AddsCatchphrase < ActiveRecord::Migration[5.1]
   #to make an additive change to a schema we create
   #a new migration, and then in the change method,
   #we make the change.
