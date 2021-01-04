6c63d032ba3cee517ce5f40eec30687a366f05fc
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-04 18:14:54 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,4 +6,10 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
 
+  def list_roles
+    characters.collect do |key|
+      "#{key.name} #{key.show.name}"
+    end
+  end
+
 end
